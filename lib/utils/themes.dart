a705fc86c363b1a6ccc417d4c720c955ba8f01d8
Madhawakavindu <madhawakavindu7@gmail.com>
2026-01-04 23:45:39 +0530

    u

---
+++ lib/utils/themes.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class ThemesModeData {
+  //light mode
+  final ThemeData lightMode = ThemeData(
+    brightness: Brightness.light,
+    primarySwatch: Colors.blue,
+    fontFamily: GoogleFonts.poppins().fontFamily,
+    appBarTheme: AppBarTheme(
+      backgroundColor: Colors.white,
+      titleTextStyle: TextStyle(
+        color: Colors.black,
+        fontFamily: GoogleFonts.poppins().fontFamily,
+        fontSize: 20,
+      ),
+      iconTheme: const IconThemeData(
+        color: Colors.black,
+      ),
+    ),
+    inputDecorationTheme: InputDecorationTheme(
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(20),
+        borderSide: const BorderSide(
+          color: Colors.black,
+        )
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(20),
+        borderSide: const BorderSide(
+          color: Colors.black,
+      ),
+    )
+    enabledBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(20),
+      borderSide: const BorderSide(
+        color: Colors.black,
+      ),
+    ),
+  ),
+  scaffoldBackgroundColor: Colors.white,
+  );
+}
